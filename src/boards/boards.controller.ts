5652721b59dc8b9ebd1f29113f6881c68a8798db
blackmount22 <moongang1022@naver.com>
2023-01-05 21:11:43 +0900

    Board DTO CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,9 +14,19 @@ export class BoardsController {
 
     @Post()
     createBoard(
-        @Body('title') title: string, 
-        @Body('description') description: string
+        @Body() createBoardDto: CreateBoardDto
     ): Board {
-        return this.boardsService.createBoard(title, description);
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    // 두개 이상 파라미터 인 경우 (@Param() params: string[])
+    getBoardByID(@Param('id') id: string) : Board {
+        return this.boardsService.getBoardByID(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id:string): void {
+        this.boardsService.deleteBoard(id);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {v1 as uuid} from 'uuid'
 import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -11,7 +12,9 @@ export class BoardsService {
     }
 
     // 게시글 생성
-    createBoard(title: string, description: string) {
+    createBoard(createBoardDto: CreateBoardDto) {
+        const {title, description} = createBoardDto;
+
         const board: Board = {
             id: uuid(),
             title,
@@ -24,5 +27,15 @@ export class BoardsService {
 
         this.boards.push(board);
         return board;
+    }
+
+    // 게시물 찾기
+    getBoardByID(id: string) : Board{
+        return this.boards.find((board) => board.id === id);
+    }
+
+    // filter 같지 않은 것만 남겨두기
+    deleteBoard(id: string) : void {
+        this.boards.filter((board) => board.id !== id);
     }
 }
