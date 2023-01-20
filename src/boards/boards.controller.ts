6ebe2b1cbd8ffc5662dce6a3a1610925ddc34110
blackmount22 <moongang1022@naver.com>
2023-01-20 16:52:12 +0900

    Entity, Repository 추가로 CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -8,35 +9,40 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
     constructor(private boardsService : BoardsService){ }
 
-    @Get('/')
-    getAllBoard(): Board[] {
-        return this.boardsService.getAllBoards()
-    }
+    // @Get('/')
+    // getAllBoard(): Board[] {
+    //     return this.boardsService.getAllBoards()
+    // }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
-        return this.boardsService.createBoard(createBoardDto);
-    }
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // createBoard(
+    //     @Body() createBoardDto: CreateBoardDto
+    // ): Board {
+    //     return this.boardsService.createBoard(createBoardDto);
+    // }
 
     @Get('/:id')
-    // 두개 이상 파라미터 인 경우 (@Param() params: string[])
-    getBoardByID(@Param('id') id: string) : Board {
+    getboardByID(@Param('id') id:number): Promise<Board>{
         return this.boardsService.getBoardByID(id);
     }
 
-    @Delete('/:id')
-    deleteBoard(@Param('id') id:string): void {
-        this.boardsService.deleteBoard(id);
-    }
+    // @Get('/:id')
+    // // 두개 이상 파라미터 인 경우 (@Param() params: string[])
+    // getBoardByID(@Param('id') id: string) : Board {
+    //     return this.boardsService.getBoardByID(id);
+    // }
 
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: string,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-    ) {
-        return this.boardsService.updateBoardStatus(id, status);
-    }
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id:string): void {
+    //     this.boardsService.deleteBoard(id);
+    // }
+
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //     @Param('id') id: string,
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+    // ) {
+    //     return this.boardsService.updateBoardStatus(id, status);
+    // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,56 +1,72 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import {v1 as uuid} from 'uuid'
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
+import { BoardRepository } from './board.repository';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-    private boards : Board[] = [];
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository:BoardRepository
+    ){}
 
-    getAllBoards(): Board[] {
-        return this.boards;
-    }
+    // getAllBoards(): Board[] {
+    //     return this.boards;
+    // }
 
-    // 게시글 생성
-    createBoard(createBoardDto: CreateBoardDto) {
-        const {title, description} = createBoardDto;
+    // // 게시글 생성
+    // createBoard(createBoardDto: CreateBoardDto) {
+    //     const {title, description} = createBoardDto;
 
-        const board: Board = {
-            id: uuid(),
-            title,
-            // title: title
-            // 과 동일한 문법
-            description,
-            //description: description 
-            status: BoardStatus.PUBLIC
+    //     const board: Board = {
+    //         id: uuid(),
+    //         title,
+    //         // title: title
+    //         // 과 동일한 문법
+    //         description,
+    //         //description: description 
+    //         status: BoardStatus.PUBLIC
+    //     }
+
+    //     this.boards.push(board);
+    //     return board;
+    // }
+
+    async getBoardByID(id: number): Promise <Board> {
+        const found = await this.boardRepository.findOne(id);
+
+        if(!found) {
+            throw new NotFoundException(`Can't find board with id ${id}`);
         }
 
-        this.boards.push(board);
-        return board;
+        return found;
     }
 
-    // 게시물 찾기
-    getBoardByID(id: string) : Board{
-         const found = this.boards.find((board) => board.id === id);
+    // // 게시물 찾기
+    // getBoardByID(id: string) : Board{
+    //      const found = this.boards.find((board) => board.id === id);
 
-         if(!found) {
-            throw new NotFoundException(`Can't find Board with id ${id}`);
-         }
+    //      if(!found) {
+    //         throw new NotFoundException(`Can't find Board with id ${id}`);
+    //      }
 
-         return found;
-    }
+    //      return found;
+    // }
 
-    // filter 같지 않은 것만 남겨두기
-    deleteBoard(id: string) : void {
-        const found = this.getBoardByID(id);
-        this.boards.filter((board) => board.id !== found.id);
-    }
+    // // filter 같지 않은 것만 남겨두기
+    // deleteBoard(id: string) : void {
+    //     const found = this.getBoardByID(id);
+    //     this.boards.filter((board) => board.id !== found.id);
+    // }
 
-    // getBoardById로 board를 찾고
-    // 찾은 board status 값을 업데이트
-    updateBoardStatus(id: string, status:BoardStatus): Board {
-        const board = this.getBoardByID(id);
-        board.status = status;
-        return board;
-    }
+    // // getBoardById로 board를 찾고
+    // // 찾은 board status 값을 업데이트
+    // updateBoardStatus(id: string, status:BoardStatus): Board {
+    //     const board = this.getBoardByID(id);
+    //     board.status = status;
+    //     return board;
+    // }
 }
