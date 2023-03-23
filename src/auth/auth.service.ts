d34e8dd6fb592bfaecafad8860c5eeae1ebbbb8d
blackmount22 <moongang1022@naver.com>
2023-03-23 17:51:07 +0900

    암호화 및 로그인

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
+import { UnauthorizedException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class AuthService {
@@ -13,5 +15,16 @@ export class AuthService {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.userRepository.findOne({username});
+
+        if(user && (await bcrypt.compare(password, user.password))){
+            return 'login success';
+        } else {
+            throw new UnauthorizedException('login Failed')
+        }
     }
 }
